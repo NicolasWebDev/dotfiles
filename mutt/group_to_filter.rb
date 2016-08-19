be546e8879cfcb8f038409040ba7782653c72df2
Sathors <sathors@free.fr>
2016-08-19 13:12:44 -0500

    [MUTT] Change the managing of coloring, using groups instead of all the addresses.

--- mutt/group_to_filter.rb
+++ mutt/group_to_filter.rb
@@ -3,4 +3,5 @@ GROUP_FILE = ARGV[0].freeze
 addresses = File.read(GROUP_FILE).split "\n"
 filters = addresses.map { |elt| "~f #{elt} | ~t #{elt}" }
 filter = filters.join ' | '
-puts filter
+#puts filter
+puts addresses.join ' '
